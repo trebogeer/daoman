b4e1cc94e162d5c28b402848c9cecf9360c9f818
dimav <trebogeer@gmail.com>
2014-07-31 17:18:32 -0700

    mysql jdbc interaction code

---
+++ daoman-mysql/src/main/resources/get_dinosaur_name.sql
@@ -0,0 +1,28 @@
+/*
+-- CALL dinosaurs.get_dinosaurs(@err, 1);
+*/
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS dinosaurs.get_dinosaur_name$$
+
+CREATE PROCEDURE dinosaurs.get_dinosaur_name(
+  OUT error_code INT,
+  OUT dname VARCHAR(40),
+  IN id BIGINT
+)
+BEGIN
+
+  SET error_code = -1;
+
+  SELECT
+    @dname := dinosaurName
+  FROM
+    dinosaurs.dinosaur
+  WHERE dinosaurId = id;
+
+  SET error_code = 0;
+
+END$$
+
+DELIMITER ;
