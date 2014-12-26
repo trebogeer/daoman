b312d9646ffefeb6869dc30d900a8531a1aef157
dimav <trebogeer@gmail.com>
2014-12-26 15:12:15 -0800

    simple jdbi example

---
+++ daoman-gen/src/test/java/com/trebogeer/daoman/JDBITest.java
@@ -0,0 +1,49 @@
+package com.trebogeer.daoman;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlConnectionPoolDataSource;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import org.skife.jdbi.v2.DBI;
+import org.skife.jdbi.v2.Handle;
+import org.skife.jdbi.v2.util.StringMapper;
+import org.testng.annotations.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * @author dimav
+ *         Date: 12/26/14
+ *         Time: 2:55 PM
+ */
+public class JDBITest {
+
+    @Test
+    public void test() throws Exception {
+       /* Class.forName("com.mysql.jdbc.Driver").newInstance();
+        Connection con = DriverManager.getConnection("jdbc:mysql://" + cfg.dbHost + ":" + cfg.dbPort + "/",
+                cfg.userName, cfg.password);*/
+        MysqlDataSource dataSource = new MysqlConnectionPoolDataSource();
+        dataSource.setUser("username");
+        dataSource.setPassword("password");
+        dataSource.setServerName("localhost");
+        dataSource.setPort(3306);
+        dataSource.setDatabaseName("databasename");
+
+        DBI dbi = new DBI(dataSource);
+
+
+        Handle h = dbi.open();
+        h.execute("create table something (id int primary key, name varchar(100))");
+
+        h.execute("insert into something (id, name) values (?, ?)", 1, "Brian");
+
+        String name = h.createQuery("select name from something where id = :id")
+                .bind("id", 1)
+                .map(StringMapper.FIRST)
+                .first();
+
+        assert name.equals("Brian");
+
+        h.close();
+    }
+}
